f79e0b9e33e5e5ac76a3ad95b4119252c8a9b607
KukimiCan <41055ht@gmail.com>
2026-09-09 12:25:41 +0900

    Use standard Next.js output on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
 
 /** @type {(phase: string) => import('next').NextConfig} */
 const nextConfig = (phase) => ({
-  distDir: phase === PHASE_DEVELOPMENT_SERVER ? '.next-dev' : '.next-build',
+  distDir: phase === PHASE_DEVELOPMENT_SERVER ? '.next-dev' : '.next',
   async rewrites() {
     const apiBaseUrl = process.env.AOZORA_API_URL;
 
